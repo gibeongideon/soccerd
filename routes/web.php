dc8a266b232a38581183af1a435df3e6cb4c563a
gibeongideon <kipngeno.gibeon@gmail.com>
2023-06-19 12:06:42 +0300

    intergrate jetstream to app

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-//use App\Models\Post;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\HomeController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,56 +10,26 @@ use App\Models\User;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-
-Route::get('/',function(User $author){
-    // dd($author);
-    //$posts = $author->post();
- 
-   // dd('TEst'.$posts->count());
- 
-     return  ['Test'=>"Yashio Kenya HOME . Please check  route '/latestposts'  ",
-    'Que'=>''];
- });
-
-
-
-
-
-Route::get('/posts/{category_id}/{author_id?}', [HomeController::class, 'index'])->name('index'); // id is the category ID
-
-
-
-
-
-Route::get('/posts', [PostController::class, 'index'])->name('home');
-
-
-Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('postdetails');
-
-Route::get('categories/{category}',function(Category $category){
-    return view('categoriesposts',[
-
-        'posts'=>$category->post,
-        'category' => $category,
-        //'categories' => Category::all(),
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-})->name('categoryposts');
+});
 
-
-Route::get('authors/{author}',function(User $author){
-   // dd($author);
-   //$posts = $author->post();
-
-  // dd('TEst'.$posts->count());
-
-    return view('postsbyauthor',[
-        'posts'=> $author->post,
-       'author' => $author,
-        
-    ]);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 });
